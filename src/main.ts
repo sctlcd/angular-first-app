76a2c48444accda7ad356601cd547cfbe996a38b
8 <lucinda.souchet@outlook.com>
2021-07-27 18:27:24 +0200

    feat: remove Joke Component

--- src/main.ts
+++ src/main.ts
@@ -16,23 +16,6 @@ import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-// // Joke Component
-@Component({
-  selector: 'joke',
-  template: `
-    <h1>{{ setup }}</h1>
-    <h2>{{ punchline }}</h2>
-  `
-})
-class JokeComponent {
-  setup: string;
-  punchline: string;
-
-  constructor() {
-    this.setup = 'What did the cheese say when it looked in the mirror?';
-    this.punchline = 'Halloumi (Hello Me)';
-  }
-}
 
 // Joke List Component
 @Component({
