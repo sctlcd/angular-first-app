007d374f665fe0e58fc0c5ad3cddcb35debecf3f
8 <lucinda.souchet@outlook.com>
2021-07-27 17:40:09 +0200

    feat: add Joke list component, display it

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
+// // Joke Component
 @Component({
   selector: 'joke',
   template: `
@@ -33,10 +34,42 @@ class JokeComponent {
   }
 }
 
+// Joke List Component
+@Component({
+  selector: 'joke-list',
+  template: `
+<div class="card card-block my-4 py-3 px-3"
+      *ngFor="let joke of jokes">
+  <h4 class="card-title">{{ joke.setup }}</h4>
+  <p class="card-text">{{ joke.punchline }}</p>
+</div>
+`
+})
+class JokeListComponent {
+  jokes: any[];
+
+  constructor() {
+    this.jokes = [
+      {
+        setup: "What did the cheese say when it looked in the mirror?",
+        punchline: "Hello-Me (Halloumi)"
+      },
+      {
+        setup: "What kind of cheese do you use to disguise a small horse?",
+        punchline: "Mask-a-pony (Mascarpone)"
+      },
+      {
+        setup: "A kid threw a lump of cheddar at me",
+        punchline: "I thought ‘That’s not very mature’"
+      },
+    ];
+  }
+}
+
 @NgModule({
   imports: [BrowserModule],
-  declarations: [JokeComponent],
-  bootstrap: [JokeComponent]
+  declarations: [JokeListComponent],
+  bootstrap: [JokeListComponent]
 })
 export class AppModule {
 }
