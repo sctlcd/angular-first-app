a2d74983b674d22b7be0f8a4516375c0165e39da
8 <lucinda.souchet@outlook.com>
2021-07-27 14:49:53 +0200

    feat: add Joke component

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+//
+// if (environment.production) {
+//   enableProdMode();
+// }
+//
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {Component} from '@angular/core';
 
-if (environment.production) {
-  enableProdMode();
+@Component({
+  selector: 'joke',
+  template: `
+    <h1>What did the cheese say when it looked in the mirror?</h1>
+    <h2>Halloumi (Hello Me)</h2>
+  `
+})
+class JokeComponent {
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [JokeComponent],
+  bootstrap: [JokeComponent]
+})
+export class AppModule {
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
